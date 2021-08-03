8ed1e4bc1d99e1d49e180ac96b2c37ccdd771a4f
Ivanpepp <nastroivan3@gmail.com>
2021-08-03 15:01:37 +0200

    js-test

---
+++ myscript.js
@@ -0,0 +1,15 @@
+//Primo ed ultimo alert 
+alert("QUESTO SARA' IL MIO PRIMO ED ULTIMO ALERT")
+console.log("Prova di Alert!!!")
+
+// Cambio Il primo h1
+document.getElementById("titolo").innerHTML = "JAVASCRIPT NON E' JAVA";
+
+//adesso los crivo nella console
+console.log("ho appena sostitituito il primo titotolo con: " + document.getElementById("titolo").innerHTML);
+
+//cambio il primo h2
+document.getElementById("sottotitolo").innerHTML = "MA ALMENO E' PIU' SEMPLICE DI C";
+console.log("ho appena sostitituito il primo sottotitotolo con: " + document.getElementById("sottotitolo").innerHTML);
+
+
